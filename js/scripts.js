7c80068dff4822ef8a0bd2782948d1982ded34b4
Piotr Sociński <psocinski@gmail.com>
2017-12-28 00:55:34 +0100

    Added solution second version

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+// First version
+
 var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.",
 	dinosaur = "triceratops".toUpperCase(),
 	replace = text.replace('Velociraptor', dinosaur),
 	replaceNew = replace.length/2;
 
 console.log(replace.substr(0,replaceNew));
+
+// Second version
+
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.",
+	dinosaur = "triceratops".toUpperCase(),
+	replace = text.replace('Velociraptor', dinosaur);
+	//replaceNew = replace.length/2;
+
+console.log(replace.substr(0,replace.length/2));
